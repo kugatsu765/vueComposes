8ad3a8f3208b38a33e49c87c72ba14dd5b2d6bb2
Romain gabel <rgabel@protonmail.com>
2021-11-30 23:25:18 +0100

    ✅ Add test for useSearch

--- src/composables/useSearch.ts
+++ src/composables/useSearch.ts
@@ -1,16 +1,23 @@
 import { ref, Ref, ComputedRef, computed } from "vue";
+import { searchInArray, searchsInArray } from "@kugatsu/utilities"
 
 interface IUseSearch<T> {
     search: Ref<string | undefined | string[]>;
     datasFiltred: ComputedRef<T[]>
 }
 
-export function useSearch<T>(datas: T[]): IUseSearch<T> {
-
+export function useSearch<T>(datas: Ref<T[]>): IUseSearch<T> {
     const search = ref<string | string[]>()
 
     const datasFiltred = computed(() => {
-        return datas
+        if (Array.isArray(search.value)) {
+            return searchsInArray(datas.value, search.value)
+        }
+        else if (typeof search.value === 'string') {
+            return searchInArray(datas.value, search.value)
+        } else {
+            throw '"keywords must be string[] or string"';
+        }
     })
 
     return { search, datasFiltred }
---
+++ test/useSearch.spec.ts
@@ -0,0 +1,85 @@
+import { useSearch } from '../src/composables/useSearch'
+import { ref } from "vue";
+
+
+describe("Search string in datas", () => {
+
+    const datas = ref([
+        { name: "Romain", age: 32, sport: ["badminton"] },
+        { name: "Aline", age: 30, sport: ["tennis", "badminton"] }
+    ])
+
+    it("should return 1 element ", () => {
+        const keywords = "tennis"
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(1);
+    });
+
+    it("should return 2 elements ", () => {
+        const keywords = "badminton"
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(2);
+    });
+
+    it("should return 1 elements ", () => {
+        const keywords = '30'
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(1);
+    });
+
+    it("should return 0 elements ", () => {
+        const keywords = '33'
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(0);
+    });
+
+
+});
+
+describe("Search multi string in datas", () => {
+
+    const datas = ref([
+        { name: "Romain", age: 32, sport: ["tennis", "badminton"] },
+        { name: "Aline", age: 30, sport: ["badminton"] }
+    ])
+
+    it("should return 0 element ", () => {
+        const keywords = ['30', 'tennis'];
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(0);
+    });
+
+    it("should return 1 elements ", () => {
+        const keywords = ['32', 'tennis'];
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(1);
+    });
+
+    it("should return 1 elements ", () => {
+        const keywords = ['tennis'];
+
+        const { datasFiltred, search } = useSearch(datas)
+        search.value = keywords
+
+        expect(datasFiltred.value.length).toEqual(1);
+    });
+
+});
